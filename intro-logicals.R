40c32be96cfe415690068ee326faae99c316db5f
vedikaS-byte <vedika.shirtekar@gmail.com>
2025-08-08 13:53:44 -0700

    logical-practice

---
+++ intro-logicals.R
@@ -0,0 +1,23 @@
+#remove from global environment
+rm(list = ls())
+
+dogs <- c(1, 10, 4, 5, 18)
+cats <- c(0,12,10,5,2)
+dogs < cats
+
+5 ==T
+# logical ands
+5 < 10 & "cat" < "dog"
+
+
+banana <- c(10, 21, 12,15,22)
+apples <- c(9, 18,16,20,30)
+apples < banana & banana > 20
+
+# logical or's
+4 < 10 | 100 <2
+
+
+
+
+
